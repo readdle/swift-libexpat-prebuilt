8fa9ae80b4bac75d6695eaa93806baf7ed0bf0e5
Andrew Druk <adruk@readdle.com>
2019-07-19 14:16:12 +0300

    Dev: add support arm64-v8a

--- build/build.sh
+++ build/build.sh
@@ -7,6 +7,10 @@ pushd "$(realpath $(dirname $0))"
     rm -rf expat
     git clone https://android.googlesource.com/platform/external/expat
 
+    pushd expat
+    	git checkout oreo-release
+    popd
+
     NDK_LIBS_OUT="../libs" ndk-build "$@"
 
     cp expat/*.h ../include
